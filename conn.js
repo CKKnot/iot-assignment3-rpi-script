a7c5b2ea5e2d3af2a5f7b18ee286300447b5e2f2
CKKnot <dalvin.cat@gmail.com>
2020-06-18 03:28:55 +0800

    First Commit

---
+++ conn.js
@@ -0,0 +1,49 @@
+var mysql = require('mysql');
+//Set your MYSQL hostname & credential here
+const host = "54.158.151.199";
+const user = "ckknot";
+const password = "12345678";
+
+var conn = mysql.createConnection({
+  connectionLimit : 1,
+  host: host,
+  user: user,
+  password: password,
+  multipleStatements: true,
+  connectTimeout: 60000
+});
+
+var query;
+
+conn.connect(function(err){
+  if(err)
+    console.error("Connection Error - conn.js: " + err);
+  else {
+    console.log("Connected to " + host)
+    query = conn.query("CREATE DATABASE IF NOT EXISTS sensorDb; USE sensorDb; CREATE TABLE IF NOT EXISTS sensor (time_stamp VARCHAR(255) PRIMARY KEY,soil_moisture INT,ambient_temperature INT,light_intensity INT, light_intensity2 INT, distance INT)", function(err, result) {
+      if(err)
+        console.error("Query Error - conn.js: cannot create database & table on targeted host: " + err);
+      else {
+        JSON.stringify(result[0].warningCount) == 0
+        ? console.log("New database created")
+        : console.warn("Database exists, no changes made")
+        JSON.stringify(result[0].warningCount) == 0
+        ? console.log("New table created")
+        : console.warn("Table exists, no changes made")
+      }
+      conn.end();
+    });
+  }
+});
+
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host: host,
+  user: user,
+  password: password,
+  multipleStatements: true,
+  timezone: "+8:00",
+  database: "sensorDb",
+  connectTimeout: 60000
+});
+module.exports = pool;
---
+++ controller.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var pool = require('./conn.js');
+const mqtt = require('mqtt');
+const moment = require('moment');
+//Set your MQTT topic & credential here
+const inTopic = 'iot-assignment3/data';
+const outTopic = 'iot-assignment3/response';
+const mqttUsername = 'pi';
+const mqttPassword = '123456';
+
+/*
+This function will manage MQTT connection and topic subscription
+*/
+var client  = mqtt.connect({host : 'localhost', username : mqttUsername, password : mqttPassword, qos : 0})
+client.on('connect', function () {
+    client.subscribe(inTopic, function (err) {
+        if (err)
+            console.error("MQTT subscribe error: " + err);
+        else
+            console.log("MQTT topic subscribed");
+    })
+});
+client.on('error', function(error) {
+    if(error)
+        console.error("MQTT connection error: " + error);
+});
+
+client.on('message', function (topic, message) {
+    console.log("MQTT Message: " + message.toString());
+    var regex = RegExp('^\\d{1,4},\\d{1,4},\\d{1,4},\\d{1,4},\\d{1,4}');
+    if((regex.test(message.toString()))) {
+        //Make our date with time +8:00 according to our Malaysian time zone
+        var dataWithTime = moment().add( 8, 'hours' ).toISOString() + "," + message.toString();
+        var splitedData =  dataWithTime.split(',');
+        
+        pool.query('INSERT INTO sensor SET time_stamp = ?, soil_moisture = ?, ambient_temperature = ?, light_intensity = ?, light_intensity2 = ?,  distance = ?', splitedData, function (error, results, fields){
+            if(error){
+                console.error("SQL Insert Error: " + error);
+                return;
+            }
+            else
+                console.log("Insert query success - controller.js");
+            //Check if soil moisture level is less than 100 out of 900+
+            if (splitedData[1] < 100){
+                client.publish(outTopic, 'dry')
+            }
+            else
+                client.publish(outTopic, 'wet')
+
+            //Check if intruder is within 10 cm out of 400
+            if (splitedData[5] < 10)
+                client.publish(outTopic, 'intruded')
+            else
+                client.publish(outTopic, 'safe');
+        })
+    }
+})
