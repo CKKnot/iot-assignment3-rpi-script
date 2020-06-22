017f4cb02390598d07fb429543492fbaf9d128a7
CKKnot <dalvin.cat@gmail.com>
2020-06-23 04:56:49 +0800

    Temporarily developing on raspberry pi 1. You now can set your MQTT broker environment by changing the code's commented constants such as database hostname/ip address & credentials and MQTT topic & credentials 2. Better error & informative messages 3. Now the script will tell you the interface types and ip addresses that you should connect your MQTT client to 4. Some code refactoring

--- conn.js
+++ conn.js
@@ -1,44 +1,54 @@
-var mysql = require('mysql');
-//Set your MYSQL hostname & credential here
-const host = "54.158.151.199";
+//Set your MYSQL hostname/address & credential here
+//-----------------------------------------
+const host = "localhost";
 const user = "ckknot";
 const password = "12345678";
-var connected = false;
+//-----------------------------------------
 
+var mysql = require('mysql');
+//SQL connection setup
 var conn = mysql.createConnection({
   connectionLimit : 1,
   host: host,
   user: user,
   password: password,
   multipleStatements: true,
-  connectTimeout: 60000
+  connectTimeout: 10000
 });
 
-var query;
-
+/*
+This function is responsible to connect to the database.
+Create database & table if it does not exist on the server.
+It will handle error by displaying error message. 
+*/
 function connectDb(){
   conn.connect(function(err){
+    console.log("Database Message: ");
     if(err)
-      console.error("Connection Error - conn.js: " + err);
+      console.error("  > Connection Error - conn.js: " + err + '\n');
     else {
-      console.log("Connected to " + host)
-      query = conn.query("CREATE DATABASE IF NOT EXISTS sensorDb; USE sensorDb; CREATE TABLE IF NOT EXISTS sensor (time_stamp VARCHAR(255) PRIMARY KEY,soil_moisture INT,ambient_temperature INT,light_intensity INT, light_intensity2 INT, distance INT)");
-        if(err)
-          console.error("Query Error:\n   Ccannot create database & table on targeted host: " + err);
+      console.log("  > Connected to " + host + '\n');
+      conn.query("CREATE DATABASE IF NOT EXISTS sensorDb; USE sensorDb; CREATE TABLE IF NOT EXISTS sensor (time_stamp VARCHAR(255) PRIMARY KEY,soil_moisture INT,ambient_temperature INT,light_intensity INT, light_intensity2 INT, distance INT)", function(error, results, fields){
+        console.log("Database Initialization: ");
+        if(error)
+          console.error("  > Query Error:\n   Ccannot create database & table on targeted host: " + error);
         else {
-          JSON.stringify(result[0].warningCount) == 0
-          ? console.log("New database created")
-          : console.warn("Database exists, no changes made")
-          JSON.stringify(result[0].warningCount) == 0
-          ? console.log("New table created")
-          : console.warn("Table exists, no changes made")
-		  connected = true;
+          JSON.stringify(results[0].warningCount) == 0
+          ? console.log("  > New database created")
+          : console.warn("  > Database exists, no changes made")
+          JSON.stringify(results[0].warningCount) == 0
+          ? console.log("  > New table created")
+          : console.warn("  > Table exists, no changes made")
         }
+        console.log('\n');
         conn.end();
+      });
     }
-   });
+  });
 }
 
+connectDb();
+//Create a sql pooling connection for handling concurrent connection & performance purposes.
 var pool = mysql.createPool({
   connectionLimit : 10,
   host: host,
@@ -47,6 +57,6 @@ var pool = mysql.createPool({
   multipleStatements: true,
   timezone: "+8:00",
   database: "sensorDb",
-  connectTimeout: 60000
+  connectTimeout: 10000
 });
 module.exports = { pool, connectDb }
--- controller.js
+++ controller.js
@@ -1,14 +1,41 @@
 'use strict';
+//Set your MQTT topic & credential here
+//------------------------------------------
+const inTopic = 'iot-assignment3/data';
+const outTopic = 'iot-assignment3/response';
+const mqttUsername = 'pi';
+const mqttPassword = '123456';
+//------------------------------------------
 
 var { pool, connectDb } = require('./conn.js');
 const mqtt = require('mqtt');
 const moment = require('moment');
 const { connect } = require('mqtt');
-//Set your MQTT topic & credential here
-const inTopic = 'iot-assignment3/data';
-const outTopic = 'iot-assignment3/response';
-const mqttUsername = 'pi';
-const mqttPassword = '123456';
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+/*
+This function will get available interface type and ip address for the MQTT client to connect to
+*/
+function GetIpAddress(){
+    var ipAdresses = "";
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) 
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            if (alias >= 1) 
+                // this single interface has multiple ipv4 addresses
+                ipAdresses += "  > " + ifname + ':' + alias + " " + iface.address + "\n";
+            else
+                // this interface has only one ipv4 adress
+                ipAdresses += "  > " + ifname + ':' + alias + " " + iface.address + "\n";
+            ++alias;
+        });
+    });
+    return ipAdresses;
+}
 
 /*
 This function will manage MQTT connection and topic subscription
@@ -18,8 +45,10 @@ client.on('connect', function () {
     client.subscribe(inTopic, function (err) {
         if (err)
             console.error("MQTT subscribe error: " + err);
-        else
-            console.log("MQTT topic subscribed");
+        else {
+            console.log("MQTT topic subscribed!\nMQTT Broker Interface types and IP Addresses for MQTT client to connect to:");
+            console.log(GetIpAddress());
+        }
     })
 });
 client.on('error', function(error) {
@@ -27,6 +56,11 @@ client.on('error', function(error) {
         console.error("MQTT connection error: " + error);
 });
 
+//On MQTT message received, we format the csv data,
+//prepare SQL query, make SQL query requests, 
+//automation on what to trigger on data inferred
+//handles any error and output error information,
+//MQTT message console output as information
 client.on('message', function (topic, message) {
     console.log("MQTT Message: " + message.toString());
     var regex = RegExp('^\\d{1,4},\\d{1,4},\\d{1,4},\\d{1,4},\\d{1,4}');
@@ -35,46 +69,21 @@ client.on('message', function (topic, message) {
         var dataWithTime = moment().add( 8, 'hours' ).toISOString() + "," + message.toString();
         var splitedData =  dataWithTime.split(',');
         
-        var query = pool.query('INSERT INTO sensor SET time_stamp = ?, soil_moisture = ?, ambient_temperature = ?, light_intensity = ?, light_intensity2 = ?,  distance = ?', splitedData);//, function (error, results, fields){
-            /*if(error){
-                console.error("SQL Insert Error: " + error);
-                return;
-            }
-            else
-                console.log("Insert query success - controller.js");
-            //Check if soil moisture level is less than 100 out of 900+
-            if (splitedData[1] < 100){
-                client.publish(outTopic, 'dry')
-            }
-            else
-                client.publish(outTopic, 'wet')
-
-            //Check if intruder is within 10 cm out of 400
-            if (splitedData[5] < 10)
-                client.publish(outTopic, 'intruded')
-            else
-                client.publish(outTopic, 'safe');
-        })*/
+        var query = pool.query('INSERT INTO sensor SET time_stamp = ?, soil_moisture = ?, ambient_temperature = ?, light_intensity = ?, light_intensity2 = ?,  distance = ?', splitedData);
         query
-            .on('fields', function(fields, index) {
-            })
             .on('error', function(error) {
-                console.error("SQL Insert Error:\n    " + error);
+                console.error("SQL Insert Error:\n  > " + error);
                 connectDb();
             })
-            .on('success', function(row, index) {
-                console.log("Insert query success:\n    " + row + "\n    at " + index);
-            });
-
+        //Check if soil moisture is below 100
         if (splitedData[1] < 100){
-            client.publish(outTopic, 'dry')
+            client.publish(outTopic, 'dry');
         }
         else
-            client.publish(outTopic, 'wet')
-
+            client.publish(outTopic, 'wet');
         //Check if intruder is within 10 cm out of 400
         if (splitedData[5] < 10)
-            client.publish(outTopic, 'intruded')
+            client.publish(outTopic, 'intruded');
         else
             client.publish(outTopic, 'safe');
     }
